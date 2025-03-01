57252513f6b85de52b68346bc4d2d7a2a487c667
Matthias Huber <wi22b112@technikum-wien.at>
2025-03-01 14:40:23 +0100

    adapted javascript updates

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
-from flask import Flask, render_template, send_from_directory
-import os
+from flask import Flask, render_template, jsonify
+import threading
+import time
 
 app = Flask(__name__)
 
-# Serve the main dashboard
+# Possible status cycles
+statuses = ["fa-check text-success", "fa-exclamation text-warning", "fa-question"]
+status_data = ["fa-check text-success"] * 15  # Adjust number based on how many icons exist
+
+def update_status():
+    """Background thread to update statuses every 5 seconds"""
+    global status_data
+    index = 0
+    while True:
+        for i in range(len(status_data)):
+            status_data[i] = statuses[(index + (i // 5)) % len(statuses)]
+        index = (index + 1) % len(statuses)
+        time.sleep(5)
+
 @app.route('/')
 def serve_dashboard():
+    """Render the main HTML page"""
     return render_template('Pubmon_Dashboard.html')
 
-# Serve static files (CSS, JS, etc.)
-@app.route('/static/<path:filename>')
-def serve_assets(filename):
-    assets_folder = os.path.join(app.root_path, 'static')
-    return send_from_directory(assets_folder, filename)
+@app.route('/status')
+def get_status():
+    """API endpoint to fetch current status values"""
+    return jsonify(status_data)
+
+# Start the background thread when Flask starts
+threading.Thread(target=update_status, daemon=True).start()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
